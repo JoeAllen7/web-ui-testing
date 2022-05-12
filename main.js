b76585c2fe5547d8c9ea681ea04cf7920ba0bd93
Joe Allen <joeallen.7@hotmail.co.uk>
2022-05-12 17:38:45 +0100

    package menu code

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var showViewButtons = true;
+var selectedPackage = 0;
+
+const packageInfo = ['Package 1', 'Package 2', 'Package 3', 'Package 4', 'Package 5'];
 
 function showView(viewName) {
 	if (viewName == 'Options') {
@@ -7,6 +10,10 @@ function showView(viewName) {
 	} else if (viewName == 'ConfirmLeave') {
 		toggleConfirmLeavePopup();
 		return;
+	}
+
+	if (viewName == 'WaitOrChoosePackage') {
+		setSelectedPackage(0);
 	}
 
 	const views = document.getElementsByClassName('view');
@@ -62,5 +69,38 @@ function toggleConfirmLeavePopup() {
 		popup.style.display = 'block';
 	} else {
 		popup.style.display = 'none';
+	}
+}
+
+function buttonNextPackage() {
+	setSelectedPackage((selectedPackage + 1) % packageInfo.length);
+}
+
+function buttonPreviousPackage() {
+	setSelectedPackage(selectedPackage == 0 ? packageInfo.length - 1 : selectedPackage - 1);
+}
+
+function setSelectedPackage(packageIndex) {
+	if (packageIndex >= packageInfo.length || packageIndex < 0) {
+		//Invalid package index
+		return;
+	}
+
+	selectedPackage = packageIndex;
+
+	// const portrait = window.matchMedia('(orientation: portrait)');
+
+	document.getElementById('packageNameCentre').innerHTML = packageInfo[packageIndex];
+
+	if (packageIndex == 0) {
+		document.getElementById('packageNameLeft').innerHTML = packageInfo[packageInfo.length - 1];
+	} else {
+		document.getElementById('packageNameLeft').innerHTML = packageInfo[packageIndex - 1];
+	}
+
+	if (packageIndex == packageInfo.length - 1) {
+		document.getElementById('packageNameRight').innerHTML = packageInfo[0];
+	} else {
+		document.getElementById('packageNameRight').innerHTML = packageInfo[packageIndex + 1];
 	}
 }
