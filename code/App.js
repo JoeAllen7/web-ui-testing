89b481c1f65c75ee48c6e5d7a41f4f82fac064a7
Joe Allen <joeallen.7@hotmail.co.uk>
2023-01-25 11:10:34 +0000

    Add files via upload

---
+++ code/App.js
@@ -0,0 +1,26 @@
+function onWindowResized(event) {
+    this.updatePlayerNameLayout();
+}
+
+function updatePlayerNameLayout() {
+    var playerNameTop = document.getElementById('playerNameTop');
+    var playerNameSeparate = document.getElementById('playerNameSeparatePanel');
+    if (!playerNameTop || !playerNameSeparate) {
+        return;
+    }
+
+    var minPageSize = Math.min(document.documentElement.clientWidth, document.documentElement.clientHeight);
+    var vminWidth = (playerNameTop.clientWidth / minPageSize) * 100;
+
+    if (vminWidth === 0) {
+        return;
+    }
+
+    // If width > 65, there is enough space to display the player name alongside tabs at the top.
+    //	Otherwise, it should be displayed in a separate panel underneath the tabs.
+    var displayAtTop = vminWidth > 65;
+    playerNameTop.style.visibility = displayAtTop ? 'visible' : 'hidden';
+    playerNameSeparate.style.visibility = displayAtTop ? 'hidden' : 'visible';
+}
+
+addEventListener('resize', this.onWindowResized);
